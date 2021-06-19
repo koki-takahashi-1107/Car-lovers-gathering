091fd8c5db56a4ef9f91e6729ea1efbbe22ae175
koki-takahashi-1107 <jubeatknit18@gmail.com>
2021-06-19 10:15:50 +0000

    楽天検索修正

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,6 +1,12 @@
 class GoodsController < ApplicationController
   def search
-    if params[:keyword]
+    if params[:keyword].blank?
+      # keyword がなければそのまま表示,検索はしない
+    elsif params[:keyword].present? && params[:keyword].length < 2
+      # keywordがあって2文字以下であればエラ-
+      redirect_to rakuten_path, notice: '2文字以上入力してください'
+    else
+      # 3: 正常検索
       @items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword])
     end
   end
